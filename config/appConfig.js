205bb4b14a050b123706ffb4043b892baa4004a1
Alabhya Pandey <alabhyap@gmail.com>
2019-02-08 04:36:43 +0530

    origin

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@ let appConfig = {};
 
 appConfig.port = 3000;
 appConfig.allowedCorsOrigin = "*";
-appConfig.env = "prod";
+appConfig.env = "dev";
 appConfig.db = {
     uri: 'mongodb://127.0.0.1:27017/meetingPlannerDB'
   }
