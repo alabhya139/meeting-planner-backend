c5441ef189349b70576616f9a117b73867a2de2e
Alabhya Pandey <alabhyap@gmail.com>
2019-02-08 15:53:24 +0530

    final commit

--- app/routes/meeting.js
+++ app/routes/meeting.js
@@ -12,13 +12,13 @@ module.exports.setRouter = (app) => {
 
     app.post(`${baseUrl}/createMeeting`, meetingsController.createEvent);
 
-    app.get(`${baseUrl}/get-meetings`, authMiddleware.isAuthorized, meetingsController.getAllMeetings);
+    app.get(`${baseUrl}/get-meetings`, meetingsController.getAllMeetings);
 
     app.get(`${baseUrl}/get-meeting-by-id/:meetingId`,authMiddleware.isAuthorized, meetingsController.getMeetingById);
 
-    app.get(`${baseUrl}/get-meetings-by-user/:userId`,authMiddleware.isAuthorized, meetingsController.getMeetingsByUser);
+    app.get(`${baseUrl}/get-meetings-by-user/:userId`,meetingsController.getMeetingsByUser);
 
-    app.get(`${baseUrl}/get-meetings-by-admin/:adminId`,authMiddleware.isAuthorized, meetingsController.getMeetingsByAdmin);
+    app.get(`${baseUrl}/get-meetings-by-admin/:adminId`, meetingsController.getMeetingsByAdmin);
 
     app.post(`${baseUrl}/editMeeting`, meetingsController.editMeeting);
 }
